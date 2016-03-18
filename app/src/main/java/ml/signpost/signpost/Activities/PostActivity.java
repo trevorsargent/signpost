ef0f8f25269f85da785cdc734217d6662d276d79
Trevor Sargent <tbird1710@gmail.com>
2016-03-17 18:54:49 -0700

    create every file ever

---
+++ app/src/main/java/ml/signpost/signpost/Activities/PostActivity.java
@@ -0,0 +1,15 @@
+package ml.signpost.signpost.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ml.signpost.signpost.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_view_activity);
+    }
+}
---
+++ app/src/main/java/ml/signpost/signpost/Models/Sign.java
@@ -0,0 +1,7 @@
+package ml.signpost.signpost.Models;
+
+/**
+ * Created by student on 3/17/16.
+ */
+public class Sign {
+}
