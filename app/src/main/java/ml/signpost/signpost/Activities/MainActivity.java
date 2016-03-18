5a0c0beb5d7e23539092a0ae5f6538316e66f50c
Trevor Sargent <tbird1710@gmail.com>
2016-03-17 19:28:11 -0700

    add bottom bar

--- app/src/main/java/ml/signpost/signpost/Activities/MainActivity.java
+++ app/src/main/java/ml/signpost/signpost/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package ml.signpost.signpost.Activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,9 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import ml.signpost.signpost.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.activity_main_bottom_navigation)
+    AHBottomNavigation mBottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        ButterKnife.bind(this);
+
+        prepBottomBar();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.activity_main_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +39,28 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+    }
+
+
+    void prepBottomBar(){
+        AHBottomNavigationItem map = new AHBottomNavigationItem("Map", R.drawable.ic_maps_place, Color.parseColor("#455C65"));
+        AHBottomNavigationItem popular = new AHBottomNavigationItem("Popular", R.drawable.ic_maps_local_bar, Color.parseColor("#455C65"));
+        AHBottomNavigationItem nearby = new AHBottomNavigationItem("Nearby", R.drawable.ic_maps_local_restaurant, Color.parseColor("#455C65"));
+
+        mBottomNav.addItem(map);
+        mBottomNav.addItem(popular);
+        mBottomNav.addItem(nearby);
+
+        mBottomNav.setDefaultBackgroundColor(R.color.colorPrimaryDark);
+
+        mBottomNav.setAccentColor(R.color.colorAccent);
+        mBottomNav.setInactiveColor(R.color.colorInactive);
+
+        mBottomNav.setColored(true);
+
     }
 
 }
